bc1ca5c7dc7a5ec19bfb4eae85d10680b665b460
Frebla <32418668+Frebla@users.noreply.github.com>
2024-04-05 01:11:46 +0300

    Update nalog.js

--- nalog.js
+++ nalog.js
@@ -1,10 +1,16 @@
-const moyNalog = require('moy-nalog')
+const NalogAPI = require('moy-nalog')
 
-const nalogAPI = new moyNalog({ username:'531004875086', password: 'VolgaSiber2010&' })
+//const nalogAPI = new moyNalog({ username:'531004875086', password: 'VolgaSiber2010&' })
 
-nalogAPI.addIncome({ name:'Предоставление информационных услуг', amount: 99.99 }).then( receipt => {
-  console.log(receipt.id, receipt.data)
+const nalog = NalogAPI({autologin:false})
+
+nalog.auth('531004875086','VolgaSiber2010&')
+
+console.log(authPromise)
+
+//nalogAPI.addIncome({ name:'Предоставление информационных услуг', amount: 99.99 }).then( receipt => {
+//  console.log(receipt.id, receipt.data)
 
   // ссылка на картинку с чеком
-  return receipt.printUrl
-}).catch(console.error)
+//  return receipt.printUrl
+//}).catch(console.error)
