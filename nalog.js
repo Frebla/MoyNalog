459f86bdbe610b4309a98f0d229af8e0d05aac6d
Frebla <32418668+Frebla@users.noreply.github.com>
2024-04-05 01:14:45 +0300

    Update nalog.js

--- nalog.js
+++ nalog.js
@@ -8,9 +8,9 @@ nalog.auth('531004875086','VolgaSiber2010&')
 
 console.log('123')
 
-//nalogAPI.addIncome({ name:'Предоставление информационных услуг', amount: 99.99 }).then( receipt => {
-//  console.log(receipt.id, receipt.data)
+nalog.addIncome({ name:'Предоставление информационных услуг', amount: 99.99 }).then( receipt => {
+  console.log(receipt.id, receipt.data)
 
   // ссылка на картинку с чеком
-//  return receipt.printUrl
-//}).catch(console.error)
+  return receipt.printUrl
+}).catch(console.error)
