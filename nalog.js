25aa20795ee3c1a1a627ef636941afce668759e4
Frebla <32418668+Frebla@users.noreply.github.com>
2024-04-14 11:54:40 +0300

    Update nalog.js

--- nalog.js
+++ nalog.js
@@ -8,10 +8,7 @@ nalog.auth('531004875086','VolgaSiber2010&')
 
 console.log('123')
 
-nalog.call('incomes', {
-    from: new Date('2024-01-01 00:00'),
-    to: new Date('2024-03-01 00:00')
-  }, 'GET').then(response => {
+nalog.call('incomes?from=2024-03-01T00:00:00.690%2B03:00&to=2024-04-14T23:59:59.690%2B03:00').then(response => {
   console.log(response)
  return 
 }).catch(console.error)
